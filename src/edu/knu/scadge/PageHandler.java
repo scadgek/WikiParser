5de1503e97ea68d1c7c87e6527a09e6e2931b841
Sergey Chupov <sergey.chupov@themidnightcoders.com>
2014-06-17 02:14:02 +0300

    last code

--- src/edu/knu/scadge/PageHandler.java
+++
@@ -1,6 +0,0 @@
-package edu.knu.scadge;
-
-public interface PageHandler
-{
-  public void handle( String title, String text );
-}
--- src/edu/knu/scadge/WikiXMLParser.java
+++
@@ -1,9 +0,0 @@
-package edu.knu.scadge;
-
-import javax.xml.stream.XMLStreamException;
-import java.io.InputStream;
-
-public interface WikiXMLParser
-{
-  public void parse( InputStream stream, WikiPageHandler wikiPageHandler );
-}
